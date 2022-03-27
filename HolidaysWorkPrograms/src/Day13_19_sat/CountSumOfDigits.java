81dc981b443dc17b959176103f4b463ac88b96df
spoorthi gowda <spoorthigowda504@gmail.com>
2022-03-27 12:50:58 +0530

    Day13-19-sat commit

---
+++ HolidaysWorkPrograms/src/Day13_19_sat/CountSumOfDigits.java
@@ -0,0 +1,19 @@
+package Day13_19_sat;
+
+public class CountSumOfDigits 
+{
+
+	public static void main(String[] args) 
+	{
+		int num=1234;
+		int sum=0;
+		while(num>0)
+		{
+			sum=sum+num%10;
+			num=num/10;
+		}
+		System.out.println("sum of digits in a number:"+sum);
+
+	}
+
+}
